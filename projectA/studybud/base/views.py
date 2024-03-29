db9d42afeb8577eab8394e6655ef0f393d59cb04
gayatrisj sj <bhsgayathri2003@gmail.com>
2024-03-29 09:47:24 +0000

    base app added

--- projectA/studybud/base/views.py
+++ projectA/studybud/base/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Home Page</h1>")
+
+def room(request):
+    return HttpResponse("<h1>Room</h1>")
