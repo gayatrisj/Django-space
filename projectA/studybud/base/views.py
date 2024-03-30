25cb074d6fe02142d4484cb20c9c70d688aaf9a0
gayatrisj sj <bhsgayathri2003@gmail.com>
2024-03-30 07:07:54 +0000

    Database model is created successfully

--- projectA/studybud/base/views.py
+++ projectA/studybud/base/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': 'lets learn web3'},
-    {'id': 2, 'name': 'python enthusiasts'},
-    {'id': 3, 'name': 'data science discussions'},
-    {'id': 4, 'name': 'AI and machine learning'},
-    {'id': 5, 'name': 'frontend development tips'},
-    {'id': 6, 'name': 'backend architecture'},
-    {'id': 7, 'name': 'cybersecurity talks'},
-    {'id': 8, 'name': 'cloud computing strategies'},
-    {'id': 9, 'name': 'blockchain innovations'},
-    {'id': 10, 'name': 'agile project management'}
-]
+# rooms = [
+#     {'id': 1, 'name': 'lets learn web3'},
+#     {'id': 2, 'name': 'python enthusiasts'},
+#     {'id': 3, 'name': 'data science discussions'},
+#     {'id': 4, 'name': 'AI and machine learning'},
+#     {'id': 5, 'name': 'frontend development tips'},
+#     {'id': 6, 'name': 'backend architecture'},
+#     {'id': 7, 'name': 'cybersecurity talks'},
+#     {'id': 8, 'name': 'cloud computing strategies'},
+#     {'id': 9, 'name': 'blockchain innovations'},
+#     {'id': 10, 'name': 'agile project management'}
+# ]
 
 
 def home(request):
+    rooms = Room.objects.all()
+    print(rooms)
     return render(request,"home.html", {'rooms': rooms})
 
 def room(request,num):
-    room = None
-    for i in rooms:
-        if i["id"] == num:
-            room = i
+    room = Room.objects.get(id=num)
+  
     context = {"room":room}
     return render(request, "room.html",context )
