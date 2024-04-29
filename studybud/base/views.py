0b9cc6414b3c8feaad16c87284a4af448f9eb7e6
javas <sivajigsivajig703@gmail.com>
2024-04-29 12:10:57 +0000

    templates created

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': 'Advanced Python Programming'},
+    {'id': 2, 'name': 'Mastering ReactJS'},
+    {'id': 3, 'name': 'Deep Dive into Machine Learning'},
+    {'id': 4, 'name': 'Data Structures and Algorithms in Python'},
+    {'id': 5, 'name': 'Frontend Development Best Practices'},
+    {'id': 6, 'name': 'Cloud Computing Essentials'}
+]
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Home Page</h1>')
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return HttpResponse('<h1>Room</h1>')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,'base/room.html',context)
