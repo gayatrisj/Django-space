8540fe3e6c4c80027c538d37871417b69a747a0a
javas <sivajigsivajig703@gmail.com>
2024-05-08 05:09:39 +0000

    mobile view  section is updated

--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -14,4 +14,6 @@ urlpatterns = [
     path('delete-message/<str:pk>/', views.deleteMessage, name="delete-message"),
     path('delete-activity/<str:pk>/', views.deleteActivity, name="delete-activity"),
     path('update-user/', views.updateUser, name="update-user"),
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
